7027c3c0cfdafca767a822721cdf1fac6d90d479
Harimaharajan <harimaharajanm@CESCHNDES-0156>
2018-11-30 20:54:38 +0530

    Possibe Test Case Methods added

---
+++ OnlineFlightSearchAPITestCases/SearchFlightTests.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+
+namespace OnlineFlightSearchAPITestCases
+{
+    public class SearchFlightTests
+    {
+        [Fact]
+        public void SearchFlight_IfStartDestinationEmpty_ReturnsValidationException()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfEndDestinationEmpty_ReturnsValidationException()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfStartDestinationValid_ReturnsTrue()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfEndDestinationValid_ReturnsTrue()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfStartDestinationInValid_ReturnsValidationException()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfEndDestinationInValid_ReturnsValidationException()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfSearchDateInValid_ReturnsValidationException()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfSearchDateValid_ReturnsTrue()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_IfStartAndEndDestinationAndSearchDateValid_ReturnsFlightDetails()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_SortByTimeAscending_ReturnsFlightDetailsinAscendingOrderOfTime()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_SortByTimeDescending_ReturnsFlightDetailsinAscendingOrderOfTime()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_SortByPriceAscending_ReturnsFlightDetailsinAscendingOrderOfPrice()
+        {
+
+        }
+
+        [Fact]
+        public void SearchFlight_SortByPriceDescending_ReturnsFlightDetailsinAscendingOrderOfPrice()
+        {
+
+        }
+    }
+}
